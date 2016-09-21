b3ebb6cf6124877c8c31e85a4f2be21adb6d4cad
lindsycarbonell <lindsaycarbonell@gmail.com>
2016-09-21 15:55:25 -0400

    scraper includes license and gradschool. csv writer functioning.

--- ncmedboard/scrape.py
+++ ncmedboard/scrape.py
@@ -1,55 +1,51 @@
-# import csv
+import csv
 import requests
 from BeautifulSoup import BeautifulSoup
+
+#EXCEPTIONS TO ADD:
+#if td_tblLicenseeInfo_1_3 text == Denied, do not do any of this
+#get the denied documents 
+
+
+
 
 url = 'http://wwwapps.ncmedboard.org/Clients/NCBOM/Public/LicenseeInformation/Details.aspx?&EntityID=3200624&PublicFile=1'
 
 response = requests.get(url)
 html = response.content
 
-list_of_rows = []
+thisDoctor = []
 
 soup = BeautifulSoup(html)
 table = soup.find('table', attrs={'id': 'ctl00_ContentPlaceHolder1_dtgPostGrad'})
 
 
-id_endings = ['_lblDateIssued', '_lblLicenseNumber', '_lblDateEnding']
-data = []
+#LICENSE AND EXPIRATION
+idEndings = ['_lblDateIssued', '_lblLicenseNumber', '_lblDateEnding']
+licenseData = []
 
-for i, ending in enumerate(id_endings):
-    data.append(soup.find('span', attrs={'id': 'ctl00_ContentPlaceHolder1' + ending}).contents[0])
-    # print data
+for i, ending in enumerate(idEndings):
+    licenseData.append(soup.find('span', attrs={'id': 'ctl00_ContentPlaceHolder1' + ending}).contents[0].encode('utf-8'))
 
-schoolsAttended = soup.find('table', attrs={'id':'ctl00_ContentPlaceHolder1_dtgMedSchool'})
-schoolString = [];
-# print schools
+# thisDoctor.append(licenseData)
 
-for row in schoolsAttended.findAll('tr'):
+
+#SCHOOL OF GRADUATION AND GRADUATION YEAR
+medicalSchool = soup.find('table', attrs={'id':'ctl00_ContentPlaceHolder1_dtgMedSchool'})
+
+for row in medicalSchool.findAll('tr'):
     for i, cell in enumerate(row.findAll('td')):
-        print 'start'
-        cellText = cell.text.replace('Year of Graduation:', '')
-        print cellText
-        if cellText == "":
-            schoolString.append(cell.text)
-            print 'empty'
+        if i == 0:
+            gradSchool = cell.text
+            # print gradSchool
+        elif i == 2:
+            gradYear = cell.text
+            # print gradYear
+
+thisDoctor.append(gradSchool)
+thisDoctor.append(gradYear)
 
 
-
-# for string in schoolString:
-#     string.replace('Year of Graduation:', '')
-
-
-
-
-
-
-# print dateIssued.contents[0]
-# print licenseNumber.contents[0]
-#
-# outfile = open('./doctors.csv', 'wb')
-# writer = csv.writer(outfile)
-# writer.writerows(list_of_rows)
-
-# for row in table.findAll('tr'):
-#     for cell in row.findAll('td'):
-#         print soup.prettify()
+outfile = open("./license.csv", "wb")
+writer = csv.writer(outfile)
+writer.writerow(thisDoctor)
